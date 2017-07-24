4b6968e62736eeb131d429cb502801f7c93e323a
BoKyung <BoKyung@192.168.0.52>
2017-07-24 10:31:23 +0900

    ㅛ화ㅓㅏ

--- Test/src/test/java/com/java/test/Test.java
+++ Test/src/test/java/com/java/test/Test.java
@@ -7,6 +7,9 @@ public class Test {
 		System.out.println("하이");
 		System.out.println("반가워");
 		System.out.println("hello man~");
-		System.out.println("hihi")
+		System.out.println("hihi");
+		System.out.println("충격!깃성구 아더를 오더라고 읽어... ");
+		System.out.println("헉!깃성구 아더를 오더라고 읽어... ");
+		
 	}
 }
